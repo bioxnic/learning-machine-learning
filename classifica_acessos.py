268bc959a4636d237f9fbc625315074952345f75
Allan "bioxnic" Presotto <allanpresottoifpr@gmail.com>
2018-12-12 22:32:29 -0200

    intro: vars categóricas

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -2,18 +2,24 @@ from dados import carregar_acessos
 
 X, Y = carregar_acessos()
 
+treino_dados = X[:90]
+treino_marcacoes = Y[:90]
+
+teste_dados = X[-9:]
+teste_marcacoes = Y[-9:]
+
 from sklearn.naive_bayes import MultinomialNB
 
 modelo = MultinomialNB()
 
-modelo.fit(X, Y)
+modelo.fit(treino_dados, treino_marcacoes)
 
-resultado = modelo.predict(X)
-diferencas = resultado - Y
+resultado = modelo.predict(teste_dados)
+diferencas = resultado - teste_marcacoes
 
 acertos = [d for d in diferencas if d == 0]
 total_de_acertos = len(acertos)
-total_de_elementos = len(X)
+total_de_elementos = len(teste_dados)
 
 taxa_acerto = 100.0 * total_de_acertos / total_de_elementos
 
